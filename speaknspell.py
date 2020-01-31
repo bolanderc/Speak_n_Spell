9dd1a441e43ba92b101bbfd8677e1188ef26dd0e
Christian <christianrbolander@gmail.com>
2020-01-31 10:43:48 -0700

    Initial commit.

---
+++ speaknspell.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 10:35:40 2020
+
+@author: christian
+"""
+
+import numpy as np
+import scipy.signal as sig
+
+class SnS:
+    def __init__{self, signal}:
+        self.signal = signal
+
+
