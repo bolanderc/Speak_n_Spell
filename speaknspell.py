1868d8c70727d54b42f995300368facb5d13ece8
Christian <christianrbolander@gmail.com>
2020-01-31 13:25:57 -0700

    Speech synthesize process completed.

--- speaknspell.py
+++ speaknspell.py
@@ -10,6 +10,7 @@ import numpy as np
 import scipy.signal as sig
 import simpleaudio as sa
 import matplotlib.pyplot as plt
+from scipy.io.wavfile import read
 
 
 class SnS:
@@ -21,9 +22,9 @@ class SnS:
     def _estimate_autocorrelation(self):
         y = self.signal_raw[:, 1]
         r_aa = sig.convolve(y, y[::-1])
-        print(self.n + self.N -1)
         self._r = r_aa[self.n - 1:self.n + self.N]
-        print(self._r)
+        plt.figure(1)
+        plt.plot(self.signal_raw[:, 0], r_aa[:self.n])
 
     def _create_YW_equations(self):
         self._A = np.zeros((self.N, self.N))
@@ -31,16 +32,17 @@ class SnS:
         for i in range(self.N):
             self._A[i, i:] = self._r[:self.N-i]
             self._A[i:, i] = self._r[:self.N-i]
-        print(self._A)
         self._b = self._r[1:]
-        print(self._b)
 
     def _solve_YW_equations(self):
         self._a = -np.linalg.solve(self._A, self._b)
-        print(self._a)
 
     def _generate_input_signal(self):
-        pass
+        f_s = 8000
+        pp = 175
+        self._y_imp = np.zeros(f_s)
+        i_imp = f_s//pp
+        self._y_imp[0:-1:i_imp] = 1.
 
     def synth_signal(self):
         self._estimate_autocorrelation()
@@ -48,13 +50,25 @@ class SnS:
         self._solve_YW_equations()
         self.filter_coeffs = np.concatenate(([1.], self._a))
         print(self.filter_coeffs)
+        self._generate_input_signal()
+        self.synth_sig = sig.lfilter([1.], self.filter_coeffs, self._y_imp)
 
     def play_sound(self):
-        pass
+        plt.figure(10)
+        plt.plot(self.synth_sig)
 
-x = np.linspace(0, 3, num=1500)
-y = np.sin(50*x)
+
+#wave_obj = sa.WaveObject.from_wave_file("./Voice Recordings/_Ah_.wav")
+#play_obj = wave_obj.play()
+#play_obj.wait_done()
+data = read("./Voice Recordings/_Ah_.wav")
+y = data[1][:, 0]
+x = np.arange(0, len(y), 1)
+#x = np.linspace(0, 3, num=1500)
+#y = np.sin(50*x)
+plt.figure(0)
 plt.plot(x, y)
 test = SnS(np.array([x, y]).T, 10)
 test.synth_signal()
 yt = test._a
+test.play_sound()
