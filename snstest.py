99d93263e6cfd0cb55ba83e5a5d188f3ea686a88
Christian Bolander <39311650+bolanderc@users.noreply.github.com>
2023-05-02 12:10:35 -0600

    Add files via upload

--- snstest.py
+++ snstest.py
@@ -14,24 +14,27 @@ from scipy.io.wavfile import read
 plt.style.use('paper')
 
 
-data = read("./Voice Recordings/Short _a_.wav")
+data = read("./Voice Recordings/Long _a_edit.wav")
 y = data[1][:, 0]
 x = np.arange(0, len(y), 1)
-#    plt.figure(0)
-#    plt.plot(x, y)
-test = speaknspell.SnS(np.array([x, y]).T, "Short _a_"[:-1], 15)
+#plt.figure(figsize=(10, 5))
+#plt.plot(x, y/y.max())
+#plt.xlim((0, 1500))
+#plt.xlabel('Number of samples')
+#plt.savefig("/home/christian/Documents/Graduate School/Sp 2020/Speak & Spell/signal.pdf")
+test = speaknspell.SnS(np.array([x, y]).T, "Long _a_"[:-1], 15)
 test.synth_signal()
-test.play_sound()
+#test.play_sound()
 
 #filenames = ["Short _a_", "Short _e_", "Short _i_", "Short _o_",
 #             "Short _u_", "Long _a_", "Long _e_", "Long _i_",
 #             "Long _o_", "Long _u_", "_Ah_"]
 #for filename in filenames:
-#    data = read("./Voice Recordings/" + filename + ".wav")
+#    data = read("./Voice Recordings/" + filename + "edit.wav")
 #    y = data[1][:, 0]
 #    x = np.arange(0, len(y), 1)
-##    plt.figure(0)
-##    plt.plot(x, y)
+#    plt.figure()
+#    plt.plot(x, y)
 #    test = speaknspell.SnS(np.array([x, y]).T, filename[:-1], 15)
 #    test.synth_signal()
 #    test.play_sound()
@@ -40,7 +43,7 @@ test.play_sound()
 
 
 
-#fnames = ['Long_a_15', 'Long_e_15', 'Long_i_15',
-#          'Long_o_15', 'Long_u_15']
-#compname = "AEIOU"
-#test.synthspeech(fnames, compname)
+fnames = ['Long_a_15', 'Long_e_15', 'Long_i_15',
+          'Long_o_15', 'Long_u_15']
+compname = "AEIOU"
+test.synthspeech(fnames, compname)
--- speaknspell.py
+++ speaknspell.py
@@ -12,6 +12,7 @@ import simpleaudio as sa
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import read, write
 import time
+import itertools
 
 
 class SnS:
@@ -38,12 +39,16 @@ class SnS:
         >>> import numpy as np
         >>> from scipy.io.wavfile import read
 
-
         >>> data = read("./Voice Recordings/Short _a_.wav")
         >>> y = data[1][:, 0]
         >>> test = speaknspell.SnS(np.array([x, y]).T, 'Short_a_15', 15)
         >>> test.synth_signal()
         >>> test.play_sound()
+
+        References
+        ----------
+        Moon, T., and Stirling, W., Mathematical Methods and Algorithms for
+        Signal Processing, Prentice Hall, 2000.
         """
         self.signal_raw = signal
         self.n = len(signal[:, 1])
@@ -53,27 +58,29 @@ class SnS:
         self.label = label
         self.pp = 175
         self.f_s = 8000
+        self.marker = itertools.cycle(('x', '+', '.', 'o', '*'))
 
     def _estimate_autocorrelation(self):
         """
-
+        Estimates the autocorrelation matrix by convolving the input signal
+        with a version of itself shifted by 180 degrees.
         """
-        y = self.signal_raw[:, 1]
-        r_aa = sig.convolve(y, y[::-1])
-        self._r = r_aa[self.n - 1:self.n + self.N]
+        y = self.signal_raw[:, 1]  # Takes signal
+        r_aa = sig.convolve(y, y[::-1])  # Convolve signal with shifted version
+        self._r = r_aa[self.n - 1:self.n + self.N]  # Find appropriate auto-
+        # correlation values
 
     def _create_YW_equations(self):
         """
-
+        Sets up the Yule-Walker equations (an all-real variant of Eq. (8.11) in
+        Moon).
         """
         self._A = np.zeros((self.N, self.N))
         self._b = np.zeros((self.N, 1))
         for i in range(self.N):
-            self._A[i, i:] = self._r[:self.N-i]
-            self._A[i:, i] = self._r[:self.N-i]
+            self._A[i, i:] = self._r[:self.N-i]  # Across the rows
+            self._A[i:, i] = self._r[:self.N-i]  # Down the columns
         self._b = self._r[1:]
-        print(self._A)
-        print(self._b)
 
     def _solve_YW_equations(self):
         """
@@ -88,12 +95,11 @@ class SnS:
         self._a = -self._durbin()
         t2 = time.time()
         print(t2 - t1)
-        print(self._a_bi - self._a)
 
     def _durbin(self):
         """
-        Solves Durbin's algorithm for Toeplitz matrices. Takes `r` of size
-        `n + 1` x `1` as an input, where `n` is the size of the solution `w`.
+        Solves Durbin's algorithm for Toeplitz matrices. Uses `self._r` of size
+        `n + 1` x `1`, where `n` is the length of the input signal minus one.
         The output of the function is `w`.
         """
         n = len(self._r) - 1
@@ -123,61 +129,125 @@ class SnS:
         Generates a periodic impulse signal with a pitch period defined by the
         user with the attribute `pp`.
         """
-        self._y_imp = np.zeros(self.f_s)
-        i_imp = self.f_s//self.pp
-        self._y_imp[0:-1:i_imp] = 1.
+        self._y_imp = np.zeros(self.f_s)  # Initialize array
+        i_imp = self.f_s//self.pp  # Finds the index step to place impulses
+        self._y_imp[0:-1:i_imp] = 1.  # Sets impulses
 
     def synth_signal(self, plot=True):
         """
+        The main function used in this class. Using the signal that was input
+        through the class instance, as well as the number of poles in the
+        all-pole filter and the label to be used in saving files, this method
+        estimates the autocorrelation function to create the Yule-Walker
+        equations. The equations are then solved and a periodic impulse signal
+        is run through a filter with the poles found through the Yule-Walker
+        equations. The output from this process is saved as `synth_sig`, which
+        is the signal that is ready to be saved as a .wav file.
 
+        See Also
+        ---------
+        scipy.signal.lfilter - Filters 1-D data with an IIR or FIR filter.
+        scipy.signal.tf2zpk - Returns zero, pole, and gain from linear filter.
         """
         self._estimate_autocorrelation()
         self._create_YW_equations()
         self._solve_YW_equations()
-        self.filter_coeffs = np.concatenate(([1.], self._a))
+        self.filter_coeffs = np.concatenate(([1.], self._a))  # Include a0
         self._generate_input_signal()
         self.synth_sig = sig.lfilter([1.], self.filter_coeffs, self._y_imp)
         [z, p, k] = sig.tf2zpk([1.], self.filter_coeffs)
 
-        # Plot pole locations
-        plt.figure()
-        plt.scatter(np.real(p), np.imag(p), color='r', marker='x')
-        t = np.linspace(0, np.pi*2., 100)
-        plt.plot(np.cos(t), np.sin(t), color='k')
-        plt.axes().set_aspect('equal')
-        plt.xlabel('Real')
-        plt.ylabel('Imag')
-        plt.grid()
-        plt.savefig('./Figures/' + self.label + '_pole_loc.pdf')
+        if plot:
+            # Plot pole locations
+            plt.figure(1)
+            plt.scatter(np.real(p), np.imag(p), color='r', marker='x')
+            t = np.linspace(0, np.pi*2., 100)
+            plt.plot(np.cos(t), np.sin(t), color='k')
+            plt.axes().set_aspect('equal')
+            plt.xlabel('Real')
+            plt.ylabel('Imag')
+            plt.grid()
+            plt.tight_layout()
+            plt.savefig('./Figures/' + self.label + '_pole_loc.pdf')
 
-        # Plot magnitude of transfer function
-        omega = np.linspace(-np.pi, np.pi)
-        sum_a = np.ones(50, dtype="complex")
-        for i in range(1, self.N):
-            sum_a += self.filter_coeffs[i]*np.exp(-i*(0 + omega*1j))
-        H_z = np.absolute(1./(1. + sum_a))
-        plt.figure()
-        plt.plot(omega, H_z, color='k')
-        plt.xlabel('$\omega$ (rad/s)')
-        plt.ylabel('$|H(z)|$')
-        plt.xlim((-np.pi, np.pi))
-        plt.savefig('./Figures/' + self.label + '_transf_mag.pdf')
+            # Plot magnitude of transfer function
+            omega = np.linspace(-np.pi, np.pi)
+            sum_a = np.ones(50, dtype="complex")
+            for i in range(1, self.N):
+                sum_a += self.filter_coeffs[i]*np.exp(-i*(0 + omega*1j))
+            H_z = np.absolute(1./(1. + sum_a))
+            plt.figure(2)
+            plt.plot(omega, H_z, color='k')
+            plt.xlabel('$\omega$ (rad/s)')
+            plt.ylabel('$|H(z)|$')
+            plt.xlim((-np.pi, np.pi))
+            plt.tight_layout()
+            plt.savefig('./Figures/' + self.label + '_transf_mag.pdf')
 
     def play_sound(self):
         """
+        Creates a .wav file from `synth_sig` and then plays the output signal.
 
+        See Also
+        ---------
+        simpleaudio.play_buffer - Start playback of audio data.
+        simpleaudio.wait_done - Waits for the playback to finish before return.
         """
-        write(self.fname, 8000, self.synth_sig)
+        write(self.fname, 8000, self.synth_sig)  # Writes .wav file
+
+        # Converts to appropriate form for playback from numpy array
         self.synth_sig *= 32767 / np.max(np.abs(self.synth_sig))
         self.synth_sig = self.synth_sig.astype(np.int16)
+
+        # Audio playback
         play_obj = sa.play_buffer(self.synth_sig, 1, 2, self.f_s)
         play_obj.wait_done()
 
     def synthspeech(self, f, compname):
         """
+        Combines multiple signals together to create a form of synthesized
+        speech. The output is then saved as a .wav file and played through the
+        audio output.
 
+        Parameters
+        ----------
+        f : array_like
+            An array containing the .wav sound files that will be combined into
+            speech.
+        compname : str
+            A label that will be used to name the output file.
+
+        Example
+        -------
+        >>> data = read("./Voice Recordings/Short _a_.wav")
+        >>> y = data[1][:, 0]
+        >>> test = speaknspell.SnS(np.array([x, y]).T, 'Short_a_15', 15)
+        >>> test.synth_signal()
+        >>> test.play_sound()
+
+        >>> names = ['Long_a_15', 'Long_e_15', 'Long_i_15',
+        >>>         'Long_o_15', 'Long_u_15']
+        >>> compname = "AEIOU"
+        >>> test.synthspeech(fnames, compname)
+
+        See Also
+        ---------
+        simpleaudio.play_buffer - Start playback of audio data.
+        simpleaudio.wait_done - Waits for the playback to finish before return.
         """
-        data = read(self._dir + f[0] + ".wav")[1]
+        data = read(self._dir + f[0] + ".wav")[1]  # Initializes with first
+        # sound
+
+        # Loops through remaining sound and concatenates all sounds together
         for i in f[1:]:
             data = np.concatenate((data, read(self._dir + i + ".wav")[1]))
-        write(self._dir + compname + ".wav", 8000, data)
+
+        write(self._dir + compname + ".wav", 8000, data)  # Writes .wav file
+
+        # Converts to appropriate form for playback from numpy array
+        self.synth_phrase = data*32767 / np.max(np.abs(data))
+        self.synth_phrase = self.synth_phrase.astype(np.int16)
+
+        # Audio playback
+        play_obj = sa.play_buffer(self.synth_phrase, 1, 2, self.f_s)
+        play_obj.wait_done()
